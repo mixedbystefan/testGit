323b43d1c12ec549cf19d94c479157ac33a7e308
Stefan Sundström <mixedbystefan@gmail.com>
2019-01-21 10:49:06 +0100

    Deleted hello world and added hej

--- RestClient_Maven/src/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-
-public class HelloWorld {
-public void Hej() {
-	System.out.println("hej");
-	System.out.println("hej again");
-}
-}
---
+++ RestClient_Maven/src/hej.java
@@ -0,0 +1,10 @@
+
+public class hej {
+
+	public static void main(String[] args) {
+		System.out.println("hej");
+		// TODO Auto-generated method stub
+
+	}
+
+}
